c01052498d9796ecf70f7d32c57527265ef1dbbd
sqlcyi2008 <123438115@qq.com>
2017-12-08 14:08:10 +0800

    提交初始化代码

--- exec_jdb.py
+++ exec_jdb.py
@@ -16,11 +16,8 @@ def write_redis(line):
     r.lpush("qicaixiang", line)
 
 
-# jdb子进程
-global proc
-
-
-def outThread(var):
+# 标准输出线程
+def stdoutThread(var):
     while True:
         global proc
         line = proc.stdout.readline()
@@ -30,12 +27,13 @@ def outThread(var):
         # print(line)
         write_redis("code->" + line)
         if 'Server startup in' in line:
-            #msg = 'trace go methods\r\n'.encode('utf-8')
+            # msg = 'trace go methods\r\n'.encode('utf-8')
             msg = 'stop at com.inspur.sw.stpptnr.cmd.StPptnRCommand:706\r\n'.encode('utf-8')
             proc.stdin.write(msg)
             proc.stdin.flush()
 
         if '断点命中:' in line:
+            print("duandian="+line)
             msg = 'locals\r\n'.encode('utf-8')
             proc.stdin.write(msg)
             proc.stdin.flush()
@@ -45,20 +43,41 @@ def outThread(var):
             proc.stdin.flush()
 
 
+# 标准输入线程
+def stdinThread(var):
+    # 等待服务器启动完成
+    time.sleep(10)
+    cmd = []
+    cmd.insert(0, 'stop at com.inspur.sw.stpptnr.cmd.StPptnRCommand:706\r\n')
+    cmd.insert(0, 'locals\r\n')
+    cmd.insert(0, 'cont\r\n')
+    while False:
+        line = cmd.pop()
+        if line:
+            global proc
+            msg = line.encode('utf-8')
+            print("jdb==" + line)
+            # proc.stdin.write(msg)
+            # proc.stdin.flush()
+            time.sleep(1)
+
+
 def formatCmd(line):
-    line=line[line.find('->')+2:].replace('\\','/')
-    java = "\""+line[:line.find('java.exe') + 8].replace('java.exe','jdb.exe')+"\""
+    line = line[line.find('->') + 2:].replace('\\', '/')
+    java = "\"" + line[:line.find('java.exe') + 8].replace('java.exe', 'jdb.exe') + "\""
     options = line[line.find('java.exe') + 8:]
-    return java+" -launch "+options
+    return java + " -launch " + options
+
 
 def main():
-    outthr = threading.Thread(target=outThread, args=(u'输出线程',))
+    inthr = threading.Thread(target=stdinThread, args=(u'输入线程',))
+    outthr = threading.Thread(target=stdoutThread, args=(u'输出线程',))
 
     global r
     line = r.get(constants.QI_JAVA_CMDLINE)
 
     command = formatCmd(line.decode(encoding="utf-8", errors="ignore"))
-    print("!!!222" +command)
+    print("!!!222" + command)
     # command = 'jdb.exe -launch -Djdk.tls.ephemeralDHKeySize=2048 -Djava.util.logging.config.file=D:\\apps\\apache-tomcat-7.0.72\\conf\\logging.properties -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager -Djava.endorsed.dirs=D:\\apps\\apache-tomcat-7.0.72\\endorsed -classpath D:\\apps\\apache-tomcat-7.0.72\\bin\\bootstrap.jar;D:\\apps\\apache-tomcat-7.0.72\\bin\\tomcat-juli.jar -Dcatalina.base=D:\\apps\\apache-tomcat-7.0.72 -Dcatalina.home=D:\\apps\\apache-tomcat-7.0.72 -Djava.io.tmpdir=D:\\apps\\apache-tomcat-7.0.72\\temp org.apache.catalina.startup.Bootstrap start'
     # command = '"C:\\Program Files\\Java\\jdk1.8.0_121\\bin\\jdb.exe" -launch -Djdk.tls.ephemeralDHKeySize=2048 -Djava.util.logging.config.file=D:\\apps\\apache-tomcat-7.0.72\\conf\\logging.properties -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager -Dfile.encoding=utf-8 -Djava.endorsed.dirs=D:\\apps\\apache-tomcat-7.0.72\\endorsed -classpath D:\\apps\\apache-tomcat-7.0.72\\bin\\bootstrap.jar;D:\\apps\\apache-tomcat-7.0.72\\bin\\tomcat-juli.jar -Dcatalina.base=D:\\apps\\apache-tomcat-7.0.72 -Dcatalina.home=D:\\apps\\apache-tomcat-7.0.72 -Djava.io.tmpdir=D:\\apps\\apache-tomcat-7.0.72\\temp org.apache.catalina.startup.Bootstrap start'
     global proc
@@ -82,6 +101,9 @@ def main():
     outthr.setDaemon(True)
     outthr.start()
 
+    inthr.setDaemon(True)
+    inthr.start()
+
     msg = 'exclude flex.*,com.microsoft.*,org.apache.*,java.*,sun.*,javax.*,org.loushang.*,com.sun.*,org.json.*,org.xml.*,edu.emory.*\r\n'.encode(
         'utf-8')
     proc.stdin.write(msg)
@@ -93,6 +115,7 @@ def main():
     process1.stdin.flush()
     '''
     outthr.join()
+    inthr.join()
 
 
 if __name__ == '__main__':
