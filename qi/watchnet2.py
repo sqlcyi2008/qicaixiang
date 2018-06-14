33a0fcfcb5b3be31c5569a3efbe8928ab2f42ed7
sqlcyi2008 <123438115@qq.com>
2018-06-14 16:54:52 +0800

    完善网络抓包功能

--- qi/watchnet2.py
+++ qi/watchnet2.py
@@ -6,36 +6,41 @@ import time
 from qi.utils import *
 from qi.constants import *
 
+
 # 抓包进程执行代码:
 def main():
     sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
-    myip = getip()
-    print("@"+myip)
+    myip = '127.0.0.1'
+    print("@" + myip)
     sniffer.bind((myip, 0))
     sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
     # receive all packages
     sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
     try:
         while True:
-            raw_buffer = sniffer.recvfrom(65535)[0]
-            ipp = dpkt.ip.IP(raw_buffer)
+            raw_buffer = sniffer.recvfrom(65535)
+            ipp = dpkt.ip.IP(raw_buffer[0])
             tt = ipp.data.__class__.__name__;
-            if tt == 'TCP' or tt == 'UDP':
-                pp = ipp.data.dport;
-                if pp != 80:
-                    continue
+            if tt == 'TCP':
+                dport = ipp.data.dport;
+                sport = ipp.data.sport;
+                dict = {'8080': '8080', '3306': '3306', '1433': '1433'}
+                tcp = ipp.data.data.decode(encoding="utf-8", errors="ignore")
+                if str(sport) in dict:
+                    try:
+                        t = time.time()
+                        # print(str(int(round(t * 1000))) + "##" + tt + ":" + str(len(str(ipp.data)))+"@"+str(dport))
+                        print("QI_NET_RECV_" + str(sport))
+                    except Exception as e:
+                        print(e)
 
-                tcp = ''
-                try:
-                    tcp = ipp.data.data.decode(encoding="utf-8", errors="ignore")
-                    t = time.time()
-                    print(str(int(round(t * 1000))) + "##" + tt + ":" + str(len(str(ipp.data)))+"@"+tcp)
-                except Exception as e:
-                    print(e)
-                if tcp.startswith('GET') or tcp.startswith('POST'):
-                    line = tcp.splitlines()[0]
-                    print(line)
-                    #write_redis("http->" + line)
+                if str(dport) in dict:
+                    try:
+                        t = time.time()
+                        # print(str(int(round(t * 1000))) + "##" + tt + ":" + str(len(str(ipp.data)))+"@"+str(dport))
+                        print("QI_NET_SEND_" + str(dport))
+                    except Exception as e:
+                        print(e)
     except KeyboardInterrupt:
         pass
     # disabled promiscuous mode
