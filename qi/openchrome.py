a68839c4ed26d12df18981324778d999218af078
sqlcyi2008 <123438115@qq.com>
2018-06-17 11:35:53 +0800

    添加启动tomcat命令

---
+++ qi/openchrome.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import os
+
+
+def main():
+    os.system('C:/Users/lichuanyi/AppData/Local/Google/Chrome/Application/chrome.exe --remote-debugging-port=9222')
+
+
+if __name__ == '__main__':
+    main()
