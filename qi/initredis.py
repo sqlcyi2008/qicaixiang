da25cd8f78db462a86fb39f1eb423736f518f8f9
sqlcyi2008 <123438115@qq.com>
2018-06-27 16:06:47 +0800

    redis自动下载、解压、添加到Windows服务中。

--- qi/initredis.py
+++ qi/initredis.py
@@ -13,15 +13,38 @@ def callback(a, b, c):
     per = 100.0 * a * b / c
     if per > 100:
         per = 100
-    print ('%.2f%%' % per)
+    print('%.2f%%' % per)
 
-path = os.path.abspath('.').replace('\\','/')
+
+
+path = os.path.abspath('.').replace('\\', '/')
+redispath = path+'/redis/'
+if os.path.exists(redispath):
+    #停止服务
+    os.system(redispath+'redis-server.exe  --service-stop')
+    os.removedirs(redispath)
+
 url = 'https://github.com/ServiceStack/redis-windows/raw/master/downloads/redis-latest.zip'
-urllib.request.urlretrieve(url,path+'/redis-latest.zip' ,callback)
-azip = zipfile.ZipFile(path+'/redis-latest.zip')
-azip.extractall(path+'/redis')
+urllib.request.urlretrieve(url, path + '/redis-latest.zip', callback)
+azip = zipfile.ZipFile(path + '/redis-latest.zip')
+azip.extractall(path + '/redis')
 
-#--service-uninstall
-#ss = path+'/redis/redis-server.exe --service-install '+path+'/redis/redis.windows-service.conf --loglevel verbos'
-#os.popen(ss)
+# 在与redis-server.exe的目录中创建server_log.txt文件，否则启动失败
+logdir = path + '/redis/Logs/'
+logfile = logdir + 'redis_log.txt'
+if not os.path.exists(logfile):
+    os.mkdir(logdir)
+    fp = open(logfile, 'w')
+    fp.close()
 
+# --service-uninstall
+ss = path + '/redis/redis-server.exe  --service-install  ' + path + '/redis/redis.windows-service.conf --port 6379'
+os.system(ss)
+
+os.system(path+'/redis/redis-server.exe  --service-start')
+
+# 在与redis-server.exe的目录中创建server_log.txt文件，否则启动失败
+# redis-server --service-install redis.windows-service.conf --port 6379
+# redis-server --service-uninstall
+# redis-server --service-start
+# redis-server --service-stop
