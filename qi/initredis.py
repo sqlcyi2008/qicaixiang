f8426c6b4a6139a642b6a2053b0a0089158941b8
sqlcyi2008 <123438115@qq.com>
2018-06-27 16:54:13 +0800

    redis自动下载、解压、添加到Windows服务中。

--- qi/initredis.py
+++ qi/initredis.py
@@ -4,6 +4,9 @@ import urllib.request
 import os
 import zipfile
 
+import shutil
+
+
 def callback(a, b, c):
     '''回调函数
     @a: 已经下载的数据块
@@ -16,18 +19,22 @@ def callback(a, b, c):
     print('%.2f%%' % per)
 
 
-
 path = os.path.abspath('.').replace('\\', '/')
-redispath = path+'/redis/'
+rediszip = path + '/redis-latest.zip'
+redispath = path + '/redis/'
 if os.path.exists(redispath):
-    #停止服务
-    os.system(redispath+'redis-server.exe  --service-stop')
-    os.removedirs(redispath)
+    # 停止服务
+    os.system(redispath + 'redis-server.exe  --service-stop')
+    #os.removedirs(redispath)
+    shutil.rmtree(redispath)
+    os.remove(rediszip)
+
+exit()
 
 url = 'https://github.com/ServiceStack/redis-windows/raw/master/downloads/redis-latest.zip'
 urllib.request.urlretrieve(url, path + '/redis-latest.zip', callback)
-azip = zipfile.ZipFile(path + '/redis-latest.zip')
-azip.extractall(path + '/redis')
+azip = zipfile.ZipFile(rediszip)
+azip.extractall(redispath)
 
 # 在与redis-server.exe的目录中创建server_log.txt文件，否则启动失败
 logdir = path + '/redis/Logs/'
@@ -41,7 +48,7 @@ if not os.path.exists(logfile):
 ss = path + '/redis/redis-server.exe  --service-install  ' + path + '/redis/redis.windows-service.conf --port 6379'
 os.system(ss)
 
-os.system(path+'/redis/redis-server.exe  --service-start')
+os.system(path + '/redis/redis-server.exe  --service-start')
 
 # 在与redis-server.exe的目录中创建server_log.txt文件，否则启动失败
 # redis-server --service-install redis.windows-service.conf --port 6379
