a22f29a7d18d602cb811d71185d4aa95f1230516
sqlcyi2008 <123438115@qq.com>
2018-06-29 14:18:34 +0800

    scapy抓包工具

---
+++ qi/net_sniff.py
@@ -0,0 +1,24 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from scapy.all import *
+
+
+def pack_callback(packet):
+    print(packet.show())
+    if packet[TCP].payload:
+        mail_packet = str(packet[TCP].payload)
+        if "user" in mail_packet.lower() or "pass" in mail_packet.lower():
+            print("Server:%s" % packet[IP].dst)
+            print("%s" % packet[TCP].payload)
+
+
+print(ifaces)
+sniff(filter="tcp port 3306 or tcp port 9090", prn=pack_callback, iface="Npcap Loopback Adapter",
+      count=0)
+
+# 11     Npcap Loopback Adapter                    127.0.0.1     00:00:00:00:00:00
+# 3      Sangfor SSL VPN CS Support System VNIC                  00:FF:0F:D9:74:30
+# 4      Intel(R) Dual Band Wireless-AC 8260       10.72.91.190  F0:D5:BF:4A:5E:93
+# 5      Bluetooth Device (Personal Area Network)                F0:D5:BF:4A:5E:97
+# 9      Intel(R) Ethernet Connection I219-V       10.9.11.107   C8:5B:76:93:52:A5
--- qi/opensqlserver.py
+++ qi/opensqlserver.py
@@ -5,7 +5,7 @@ import os
 
 def main():
     print(os.popen('sqlcmd -S LICHUANYI02\SQLEXPRESS -d demo -U sa -P 123456a? -Q "sp_help student"').read())
-    print(os.popen('sqlcmd -S LICHUANYI02\SQLEXPRESS -d demo -U sa -P 123456a? -Q "select * from student"').read())
+    print(os.popen('sqlcmd -S LICHUANYI02\SQLEXPRESS -d demo -U sa -P 123456a? -Q "select name from sysobjects where xtype=\"u\""').read())
 
 
 if __name__ == '__main__':
