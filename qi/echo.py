472fdc36a162f824565b60e310657aca1799aa86
sqlcyi2008 <123438115@qq.com>
2018-11-30 10:37:07 +0800

    完善代码

---
+++ qi/echo.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+from sys import stdout
+from time import sleep
+
+# Count from 1 to 10 with a sleep
+for count in range(0, 10):
+  print(count + 1)
+  stdout.flush()
+  sleep(3)
---
+++ qi/websocketd.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import os
+
+
+def main():
+    os.system('D:/dev/PycharmProjects/qicaixiang/qi/tools/websocketd/websocketd.exe  --port=8088 python.exe D:/dev/PycharmProjects/qicaixiang/qi/echo.py')
+
+
+if __name__ == '__main__':
+    main()
